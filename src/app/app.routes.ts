84d363b966ed910f2698de22ded7e2acaeacfce0
Adi Saric <adi.saric02gmail.com>
2025-09-10 13:58:22 +0200

    added a button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Zone } from './zone/zone';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'zone', component: Zone},
+  {path: '', redirectTo: '/zone', pathMatch: 'full'},
+];
