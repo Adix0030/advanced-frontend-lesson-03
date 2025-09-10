7797b913ac48b00a103c5f32ee442f6a6046a694
Adi Saric <adi.saric02gmail.com>
2025-09-10 14:16:45 +0200

    counter

--- src/app/zone/zone.ts
+++ src/app/zone/zone.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './zone.css'
 })
 export class Zone {
+likeAngular = true;
+  count = 0;
+  counter: any;
 
+  toggle() {
+    this.likeAngular = !this.likeAngular;
+  }
+ 
+  startCounter() {
+    this.counter = setInterval(() => {
+      console.log(this.count++);
+    }, 1000);
+  }
+
+  stopCounter() {
+    clearInterval(this.counter);
+  }
 }
